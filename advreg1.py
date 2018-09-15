59685d90d5fe268329f16f9184c91433e896e55d
tushita_singh <36401416+tush05tgsingh@users.noreply.github.com>
2018-09-15 11:37:57 +0530

    6th
    keywords and intent used to respond to different types of greetings

---
+++ advreg1.py
@@ -0,0 +1,58 @@
+bot_template = "BOT : {0}"
+user_template = "USER : {0}"
+
+import re
+
+keywords = {'goodbye':['bye','farewell'],
+ 'greet':['hello','hi','hey'],
+ 'thankyou':['thank','thx']}
+
+
+responses = {'default': 'default message',
+ 'goodbye': 'goodbye for now',
+ 'greet': 'Hello you! :)',
+ 'thankyou': 'you are very welcome'}
+
+
+# Define a dictionary of patterns
+patterns = {}
+
+# Iterate over the keywords dictionary
+for intent, keys in keywords.items():
+    # Create regular expressions and compile them into pattern objects
+    patterns[intent] = '|'.join(keys)
+
+# Print the patterns
+print(patterns)
+
+# Define a function to find the intent of a message
+def match_intent(message):
+    matched_intent = None
+    for intent, pattern in patterns.items():
+        # Check if the pattern occurs in the message
+        if re.search(pattern,message):
+            matched_intent =intent
+    return matched_intent
+
+# Define a respond function
+def respond(message):
+    # Call the match_intent function
+    intent = match_intent(message)
+    # Fall back to the default response
+    key = "default"
+    if intent in responses:
+        key = intent
+    return responses[key]
+
+def send_message(message):
+    # Print user_template including the user_message
+    print(user_template.format(message))
+    # Get the bot's response to the message
+    response = respond(message)
+    # Print the bot template including the bot's response.
+    print(bot_template.format(response))
+
+# Send messages
+send_message("hello!")
+send_message("bye byeee")
+send_message("thanks very much!")
